2ee1e8a705a1a97aacfc6cf9efbc642b3d6b25f9
Sana Deghat <Sana.Deghat@gmail.com>
2026-05-09 23:21:09 -0700

    guess who has a working pause menu?
    looks ugly af tho

--- pong/main_page.gd
+++ pong/main_page.gd
@@ -1,4 +1,5 @@
 extends Node2D
+@onready var pause_menu: MarginContainer = $pause_menu
 
 var blueScore=0; 
 var orangeScore=0; 
@@ -13,11 +14,33 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _input(event):
+	if Input.is_action_just_pressed("pause_action"):
+		if pause_menu.visible==false:
+			print("on")
+			pause_menu.visible=true;
+			get_tree().paused = true
+		else:
+			print("off")
+			pause_menu.visible=false;
+			get_tree().paused = false
+
+
 
 func _ball_score(score: int) -> void:
 	if score>0:
-		
 		blueScore+=1; 
-		
 	else: 
 		orangeScore+=1;
+
+
+func _on_restart_btn_pressed() -> void:
+	pause_menu.visible=false
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://main_page.tscn")
+
+
+func _on_quit_btn_pressed() -> void:
+	pause_menu.visible=false
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://starter_menu.tscn")
---
+++ pong/pause_menu.gd
@@ -0,0 +1,20 @@
+extends MarginContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_resume_btn_pressed() -> void:
+	visible=false;
+	get_tree().paused = false
+	
+func _input(event):
+	if Input.is_action_just_pressed("pause_action"):
+		get_tree().paused = false
