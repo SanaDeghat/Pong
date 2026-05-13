eba8caaa0c0a12544a6fd9707aff01b35d8ed21c
Sana Deghat <Sana.Deghat@gmail.com>
2026-05-13 14:53:02 -0700

    particles

--- pong/ball.gd
+++ pong/ball.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 @export var START_SPEED: float = 500.0
-@export var MAX_SPEED: float = 1500.0
+@export var MAX_SPEED: float = 1200.0
 @export var SPEED_ACCEL: float = 40.0
+@export var MAX_Y_ANGLE: float = 0.8 
+@onready var ball_paticles: CPUParticles2D = $CPUParticles2D
 
 var current_speed: float = START_SPEED
 var direction: Vector2 = Vector2.ZERO
@@ -13,7 +15,6 @@ func _ready() -> void:
 	randomize()
 	reset(-1) 
 	
-	
 func _physics_process(delta: float) -> void:
 	velocity = direction * current_speed
 	move_and_slide()
@@ -23,8 +24,10 @@ func _physics_process(delta: float) -> void:
 	
 	if global_position.y > 330 and direction.y > 0:
 		direction.y *= -1
+		add_tini_spin()
 	elif global_position.y < -330 and direction.y < 0:
 		direction.y *= -1
+		add_tini_spin()
 
 func reset(location: float) -> void:
 	global_position = Vector2(450, 0)
@@ -32,18 +35,26 @@ func reset(location: float) -> void:
 	
 	var dir_x = 1 if location < 0 else -1
 	var dir_y = randf_range(-0.4, 0.4) 
-	
+	ball_paticles.lifetime = 0.7
 	direction = Vector2(dir_x, dir_y).normalized()
 	score.emit(int(location))
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	var ho = global_position.y - body.global_position.y
-	
 	var ni = ho / 50.0 
-	
+	ni = clamp(ni, -MAX_Y_ANGLE, MAX_Y_ANGLE)
+	particle()
+
 	direction.x *= -1
 	direction.y = ni
+	add_tini_spin()
 	direction = direction.normalized() 
 	current_speed = min(current_speed + SPEED_ACCEL, MAX_SPEED)
-	
-	
+
+func add_tini_spin():
+	direction.y += randf_range(-0.05, 0.05)
+	direction = direction.normalized()
+
+func particle():
+	ball_paticles.emitting = true
+	ball_paticles.direction = direction
